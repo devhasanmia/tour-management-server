07d88ba444230420ff8cd32831ea4c19c4984627
devhasanmia <dev.hasanmia@gmail.com>
2025-07-13 22:59:24 +0600

    added User Interface and Model

---
+++ src/modules/user/user.interface.ts
@@ -0,0 +1,38 @@
+import { Types } from "mongoose";
+
+export enum Role {
+    SUPER_ADMIN = "SUPER_ADMIN",
+    ADMIN = "ADMIN",
+    GUIDE = "GUIDE",
+    USER = "USER",
+}
+
+export interface IAuthProvider {
+    provider: string;
+    providerid: string;
+}
+
+export enum IsActive {
+    ACTIVE = "ACTIVE",
+    INACTIVE = "INACTIVE",
+    PENDING = "PENDING",
+    SUSPENDED = "SUSPENDED",
+    BANNED = "BANNED",
+}
+
+
+export interface IUser {
+    name: string;
+    email: string;
+    password?: string;
+    phone?: string;
+    picture: string;
+    address?: string;
+    isDeleted?: string;
+    isActive?: IsActive;
+    isVerified?: string;
+    role: Role;
+    auths: IAuthProvider[];
+    bookings?: Types.ObjectId[];
+    guides?: Types.ObjectId[]
+}
