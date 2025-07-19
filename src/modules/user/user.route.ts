b99f6e16a0dbafeea7892e5d1795072a9400333e
devhasanmia <dev.hasanmia@gmail.com>
2025-07-19 21:00:47 +0600

    Server updated

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,12 +1,39 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { UserController } from "./user.controller";
 import { validateRequest } from "../../middleware/validateRequest";
 import { UserSchema } from "./user.validation";
-
+import AppError from "../../errorHandler/AppError";
+import { verifyToken } from "../../utils/jwt";
+import config from "../../config";
+import { JwtPayload } from "jsonwebtoken";
+declare global {
+  namespace Express {
+    interface Request {
+      user?: JwtPayload;
+    }
+  }
+}
 const router = Router();
 
+const checkAuth = (...authRoles: string[]) => async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const accessToken = req.headers.authorization;
+        if (!accessToken) {
+            throw new AppError(403, "No Token Recieved")
+        }
+        const verifiedToken = verifyToken(accessToken, config.jwt.secret) as JwtPayload;
+        if (!authRoles.includes(verifiedToken.role)) {
+            throw new AppError(403, "You are not permitted to view this route!!!")
+        }
+        req.user = verifiedToken 
+        next()
+    } catch (error) {
+        console.log("jwt error", error);
+        next(error)
+    }
+}
 
 router.post("/register", validateRequest(UserSchema), UserController.createUser);
-router.get("/get-all-user", UserController.getAllUser);
+router.get("/get-all-user", checkAuth("USER"), UserController.getAllUser);
 
 export const UserRoutes = router
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -3,13 +3,14 @@ import { IAuthProvider, IUser } from "./user.interface";
 import User from "./user.model";
 import httpStatus from "http-status-codes"
 import bcrypt from "bcryptjs";
+import config from "../../config";
 const createUser = async (payload: Partial<IUser>) => {
     const { email, password, ...rest } = payload
     const isUserExist = await User.findOne({ email });
     if (isUserExist) {
         throw new AppError(httpStatus.BAD_REQUEST, "User Already Exist")
     }
-    const hashPassword = await bcrypt.hash(password as string, 10);
+    const hashPassword = await bcrypt.hash(password as string, config.bcrypt.salt_rounds);
     const authProvider: IAuthProvider = { provider: "credentials", providerid: email as string }
     const user = await User.create({
         email,
